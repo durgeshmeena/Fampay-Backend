58ffefe099d13987586571a944e03c87abe782fc
durgeshmeena <durgeshkumar@iitg.ac.in>
2021-12-23 23:43:49 +0530

    adding comments

--- user/db.py
+++ user/db.py
@@ -3,8 +3,10 @@ from os import environ, path
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+# getting SECRETE Mongo_URI
 mongoURI = environ.get('MONGO_URI')
 
+# connecting to database
 def configDB():
     print('connecting to database...')
     client = pymongo.MongoClient(mongoURI, serverSelectionTimeoutMS=30000)
